b5b1584f4a0b483731d644748d7771349d42cd18
loroque <loroque@LOROQUE-BR.br.oracle.com>
2020-11-18 21:59:32 -0300

    incluir get

--- src/main/java/br/com/webinar/cicd/controller/CalculadoraController.java
+++ src/main/java/br/com/webinar/cicd/controller/CalculadoraController.java
@@ -1,5 +1,6 @@
 package br.com.webinar.cicd.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,5 +17,10 @@ public class CalculadoraController {
     public double calcular(@RequestBody CalculoSimples detalhes){
      CalculadoraSimples cal = new CalculadoraSimples();
         return cal.calcular(detalhes);
+    }
+
+    @GetMapping
+    public String calcular(){
+        return "Webinar Oracle, seja bem vindo! #CR";
     }
 }
